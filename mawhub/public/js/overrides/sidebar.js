4b94e65ae062d5961ed60248cdb9cabb07ef87d5
darwishdev <a.darwish.dev@gmail.com>
2026-02-18 19:53:57 +0200

    add expensides to overriden workspaces

--- mawhub/public/js/overrides/sidebar.js
+++ mawhub/public/js/overrides/sidebar.js
@@ -16,7 +16,7 @@ frappe.ui.Sidebar = class Sidebar extends frappe.ui.Sidebar {
             this.sidebar_header = new frappe.ui.SidebarHeader(this);
             this.make_sidebar();
         }
-        const overriden_docs = ["people" , "crm" , "projects" , "selling" , "recruitment" ,"build"]
+        const overriden_docs = ["people" , "crm" , "expenses" , "projects" , "selling" , "recruitment" ,"build"]
         const is_overriden = overriden_docs.includes(workspace_title.toLowerCase())
         original_setup(is_overriden ? 'Mawhub' : workspace_title)
         console.log("workspace title is" , workspace_title)
